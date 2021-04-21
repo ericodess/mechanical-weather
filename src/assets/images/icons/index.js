311fe1c827f1eda7f21475994a0fba466ba30a9b
pepeyen <erickfcaldeira@gmail.com>
2021-04-21 04:31:59 -0300

    Removal of inline fill value from all svg images

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -1,10 +1,15 @@
-export {default as closeIcon} from './top-frame/cancel';
+//Navbar Icons
+export {default as settingsIcon} from './navbar/settings';
+export {default as temperatureIcon} from './navbar/temperature';
+
+//Weather Icons
 export {default as clearDayIcon} from './weather/clear-day';
 export {default as clearNightIcon} from './weather/clear-night';
 export {default as cloudIcon} from './weather/cloud';
 export {default as cloudyDayIcon} from './weather/cloudy-day';
 export {default as cloudyNightIcon} from './weather/cloudy-night';
-export {default as minusIcon} from './top-frame/minus';
 export {default as rainingIcon} from './weather/raining';
-export {default as settingsIcon} from './navbar/settings';
-export {default as temperatureIcon} from './navbar/temperature';
+
+//Misc Icons
+export {default as closeIcon} from './top-frame/cancel';
+export {default as minusIcon} from './top-frame/minus';
