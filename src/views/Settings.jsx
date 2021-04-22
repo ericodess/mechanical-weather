239d93c62c9cf09ea5fd6e65627df3cddcf74cbb
pepeyen <erickfcaldeira@gmail.com>
2021-04-22 02:25:52 -0300

    Major Settings page design and functionality update

--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -1,10 +1,52 @@
+import { useSelector } from 'react-redux';
+
 //Components
-import { Page } from '../components';
+import {
+    DropdownList,
+    InfoDiplay,
+    Page
+} from '../components';
+
+//Actions
+import { updatePortInfo } from '../actions';
+
+const electron = window.require('electron');
 
 const Settings = () => {
+    const selectedPortInfo = useSelector(state => state.portInfo),
+          fetchedList = electron.ipcRenderer.sendSync('get-port-list');
+          
+    const generatePortList = () => {
+        const newPortList = [];
+
+        fetchedList.forEach(port => {
+            newPortList.push(port.path);
+        });
+
+        return newPortList;
+    };
+
+    const insertPortInfo = (option) => {
+        const portInfo = fetchedList.find(currentPort => currentPort.path === option);
+        let port = {};
+
+        if(typeof portInfo === 'object'){
+            port = portInfo;
+        };
+
+        return updatePortInfo(port);
+    };
+
     return(
         <Page>
-            Settings
+            <DropdownList
+                options={generatePortList()}
+                action={insertPortInfo}
+            />
+            <InfoDiplay
+                info={selectedPortInfo.path ? electron.ipcRenderer.sendSync('get-port-info', selectedPortInfo.path) : {}}
+                errorMessage="Escolha uma porta"
+            />
         </Page>
     );
 };
