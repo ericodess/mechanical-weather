a254b12afff47eb070e862579b9c8462a41761f2
pepeyen <erickfcaldeira@gmail.com>
2021-04-22 02:23:16 -0300

    Minor icons file structure update

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -1,3 +1,9 @@
+//Helper Icons
+export {default as arrowIcon} from './helpers/arrow';
+export {default as closeIcon} from './helpers/cancel';
+export {default as minusIcon} from './helpers/minus';
+export {default as unknownIcon} from './helpers/unknown';
+
 //Navbar Icons
 export {default as settingsIcon} from './navbar/settings';
 export {default as temperatureIcon} from './navbar/temperature';
@@ -9,7 +15,3 @@ export {default as cloudIcon} from './weather/cloud';
 export {default as cloudyDayIcon} from './weather/cloudy-day';
 export {default as cloudyNightIcon} from './weather/cloudy-night';
 export {default as rainingIcon} from './weather/raining';
-
-//Misc Icons
-export {default as closeIcon} from './top-frame/cancel';
-export {default as minusIcon} from './top-frame/minus';
