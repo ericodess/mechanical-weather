e12f82719a498c224255621b93a846feeba6729e
pepeyen <erickfcaldeira@gmail.com>
2021-04-23 03:06:48 -0300

    Introduced multi flex-directions via props

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,6 +1,6 @@
 const Page = (props) => {
     return(
-        <section className="page --full-width --full-height --flex --row --centralize-vert">
+        <section className={`page --full-width --full-height --flex --${props.column ? 'column' : 'row'} --centralize-vert`}>
             {props.children}
         </section>
     );
