53763231e610b53b89e6ff18758f840d6e2157fd
pepeyen <erickfcaldeira@gmail.com>
2021-04-24 03:16:01 -0300

    Introduced custom availability to the Info Display component

--- src/components/InfoDisplay.jsx
+++ src/components/InfoDisplay.jsx
@@ -2,27 +2,35 @@
 import { convertCamelToPhrase } from '../services';
 
 const InfoDisplay = (props) => {
-    if(Object.entries(props.info).length > 0){
+    if(props.custom){
         return(
-            <ul className="info-display --flex --column --centralize-vert">
-                {Object.entries(props.info).map((info, index) => {
-                    return(
-                        <li
-                            key={index}
-                            className="info-display__text --flex --row --full-width"
-                        >
-                            <span>{convertCamelToPhrase(info[0])}:</span><span>{info[1] ? info[1] : '-'}</span>
-                        </li>
-                    );
-                })}
-            </ul>
-        );
-    }else{
-        return(
-            <div className="info-display --flex --column --centralize">
-                <span className="info-display__feedback">{props.errorMessage}</span>
+            <div className="info-display --flex --centralize">
+                {props.children}
             </div>
         );
+    }else{
+        if(Object.entries(props.info).length > 0){
+            return(
+                <ul className="info-display --flex --column --centralize-vert">
+                    {Object.entries(props.info).map((info, index) => {
+                        return(
+                            <li
+                                key={index}
+                                className="info-display__text --flex --row --full-width"
+                            >
+                                <span>{convertCamelToPhrase(info[0])}:</span><span>{info[1] ? info[1] : '-'}</span>
+                            </li>
+                        );
+                    })}
+                </ul>
+            );
+        }else{
+            return(
+                <div className="info-display --flex --column --centralize">
+                    <span className="info-display__feedback">{props.errorMessage}</span>
+                </div>
+            ); 
+        };
     };
 };
 
