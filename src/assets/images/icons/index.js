5496f40164fabfb51fd9e66828284ceac18c3f07
pepeyen <erickfcaldeira@gmail.com>
2021-04-25 03:14:22 -0300

    Minor icons content update

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -1,19 +1,20 @@
 //Helper Icons
-export {default as arrowIcon} from './helpers/arrow';
-export {default as closeIcon} from './helpers/cancel';
-export {default as minusIcon} from './helpers/minus';
-export {default as unknownIcon} from './helpers/unknown';
+export { default as arrowIcon } from './helpers/arrow';
+export { default as closeIcon } from './helpers/cancel';
+export { default as enterIcon } from './helpers/enter';
+export { default as minusIcon } from './helpers/minus';
+export { default as unknownIcon } from './helpers/unknown';
 
 //Navbar Icons
-export {default as displayIcon} from './navbar/display';
-export {default as settingsIcon} from './navbar/settings';
-export {default as temperatureIcon} from './navbar/temperature';
-export {default as usbIcon} from './navbar/usb';
+export { default as displayIcon } from './navbar/display';
+export { default as profileIcon } from './navbar/profile';
+export { default as settingsIcon } from './navbar/settings';
+export { default as temperatureIcon } from './navbar/temperature';
+export { default as usbIcon } from './navbar/usb';
 
 //Weather Icons
-export {default as clearDayIcon} from './weather/clear-day';
-export {default as clearNightIcon} from './weather/clear-night';
-export {default as cloudIcon} from './weather/cloud';
-export {default as cloudyDayIcon} from './weather/cloudy-day';
-export {default as cloudyNightIcon} from './weather/cloudy-night';
-export {default as rainingIcon} from './weather/raining';
+export { default as clearDayIcon } from './weather/clear-day';
+export { default as clearNightIcon } from './weather/clear-night';
+export { default as cloudyDayIcon } from './weather/cloudy-day';
+export { default as cloudyNightIcon } from './weather/cloudy-night';
+export { default as rainingIcon } from './weather/raining';
