671646f1936524dece50c8d3b95b4321e5453bb3
pepeyen <erickfcaldeira@gmail.com>
2021-04-25 03:12:45 -0300

    Follow up to the redux logic update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,5 @@
 export { default as updatePortInfo } from './updatePortInfo';
 export { default as updateScreenResolution } from './updateScreenResolution';
+export { default as updateUsername } from './updateUsername';
+export { default as updateUserLocation } from './updateUserLocation';
+export { default as updateWeatherInfo } from './updateWeatherInfo';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,16 @@ import { combineReducers } from 'redux';
 //Reducers
 import portInfoReducer from './portInfoReducer';
 import screenResolutionReducer from './screenResolutionReducer';
+import usernameReducer from './usernameReducer';
+import userLocationReducer from './userLocationReducer';
+import weatherInfoReducer from './weatherInfoReducer';
 
 const allReducers = combineReducers({
     portInfo: portInfoReducer,
-    screenResolution: screenResolutionReducer
+    screenResolution: screenResolutionReducer,
+    userInfo: usernameReducer,
+    userLocation: userLocationReducer,
+    weatherInfo: weatherInfoReducer
 });
 
 export default allReducers;
